75a38385406ae55b1261426e1bdab853f7c4ab6a
Jerry Kankelborg <jkankelborg@csumb.edu>
2019-04-03 03:19:47 +0000

    adding

--- hmwk/hw3/api/firstQ.php
+++ hmwk/hw3/api/firstQ.php
@@ -4,12 +4,14 @@ $first = array();
 
 $first[0] = array();
 $first[0]['answer']= "sacramento";
-$first[0]['question']= "What's the capitol of California?";
+$first[0]['question']= "What's the capital of California?";
+
 
 
 $first[1] = array();
 $first[1]['answer']= "new york";
-$first[1]['question']= "What's the capitol of new york?";
+$first[1]['question']= "What's the capital of new york?";
+
 
 $randomNum = rand(0,1);
 
