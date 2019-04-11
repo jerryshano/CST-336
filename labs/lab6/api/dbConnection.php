f17308a11b8d243805ef9bf767080e9e6d3fcc8f
Jerry Kankelborg <jkankelborg@csumb.edu>
2019-04-11 17:28:28 +0000

    adding

--- labs/lab6/api/dbConnection.php
+++ labs/lab6/api/dbConnection.php
@@ -9,7 +9,17 @@ function getDatabaseConnection($dbname = 'ottermart'){
  //creates data base connection
  $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
  
- //displays errors when accessing tables
+  //when connecting from Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+ 
+ $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
  $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
  
  return $dbConn;
