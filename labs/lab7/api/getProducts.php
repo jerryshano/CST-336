21baca92d1ad414c7a2f6c4451153a4daddb4a82
Jerry Kankelborg <jkankelborg@csumb.edu>
2019-04-13 23:39:32 +0000

    add

---
+++ labs/lab7/api/getProducts.php
@@ -0,0 +1,17 @@
+<?php
+
+ include 'dbConnection.php';
+  $conn = getDatabaseConnection("ottermart");
+
+$sql = "SELECT * FROM om_product ORDER BY productPrice LIMIT 10";
+$stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
+$stmt->execute();
+$records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
+
+// print_r($records); // displays array content. 
+
+echo json_encode($records); 
+
+// echo $records[0]['productName']; // 
+
+?>
